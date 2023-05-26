736ed1970feee54ecbddbd65a7104d098774d340
AliceCaselli <alicecaselli3@gmail.com>
2023-05-26 17:32:51 +0200

    first commit

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container">
+        <h1>Tutti i post</h1>
+
+        <table class="table table-striped">
+            <thead>
+                <th>
+                    Titolo
+                </th>
+                <th>
+                    Slug
+                </th>
+                <th>
+
+                </th>
+            </thead>
+            <tbody>
+                @foreach ($posts as $post)
+                    <tr>
+                        <td>
+                            {{$post->title}}
+                        </td>
+                        <td>
+                            {{$post->slug}}
+                        </td>
+                        <td>
+                            <a href="{{route('admin.posts.show', $post)}}">Apri</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
